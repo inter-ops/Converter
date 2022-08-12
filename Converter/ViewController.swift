14cd6897c7ed607745a84246f52ab1c1dcd5f118
Justin Bush <revblaze@users.noreply.github.com>
2022-08-12 16:30:14 -0600

    Switch default presentation to Storyboard
    Backwards compatibility for macOS 10.15

---
+++ Converter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Converter
+//
+//  Created by Justin Bush on 8/12/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
