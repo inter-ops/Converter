abc0d4a1e2f0736c5f9770ec4a23fa914ef03091
Justin Bush <revblaze@users.noreply.github.com>
2022-08-12 16:22:03 -0600

    Import new project → macOS 10.15

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Justin Bush on 8/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
