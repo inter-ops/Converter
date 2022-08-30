1cc17ec663fd8e266aa76c45c3fa0e000df01539
Justin Bush <revblaze@users.noreply.github.com>
2022-08-30 11:01:35 -0600

    feat: implement custom objects, classes and corresponding ViewControllers

---
+++ Converter/Views/Popover/HelpInfoViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HelpInfoViewController.swift
+//  Converter
+//
+//  Created by Justin Bush on 8/30/22.
+//
+
+import Cocoa
+
+class HelpInfoViewController: NSViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do view setup here.
+  }
+  
+  @IBAction func contactButtonAction(sender: NSButton) {
+    print("Contact button was clicked")
+    // TODO: Implement mailto: hello@airtv.io
+    
+  }
+  
+}
