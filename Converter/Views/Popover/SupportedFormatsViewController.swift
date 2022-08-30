dbcde6f81891a824808c8c6937efcd0d8becda8d
Justin Bush <revblaze@users.noreply.github.com>
2022-08-29 18:18:24 -0600

    feat: new popover directory ref, controller

---
+++ Converter/Views/Popover/SupportedFormatsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SupportedFormatsViewController.swift
+//  Converter
+//
+//  Created by Justin Bush on 8/29/22.
+//
+
+import Cocoa
+
+class SupportedFormatsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
