b2d513dba5c360332fc245a56d37da7b71169281
Justin Bush <revblaze@users.noreply.github.com>
2022-09-04 12:13:12 -0600

    feat: send error log contents as attachment

--- Converter/Views/Alerts/ErrorPrompt.swift
+++ Converter/Views/Alerts/ErrorPrompt.swift
@@ -22,11 +22,15 @@ extension ViewController {
       if modalResponse == NSApplication.ModalResponse.alertFirstButtonReturn {
         print("User did choose to send error message")
         
+        let messageContents = "Please make sure to include the text file as an attachment, as well as any additional details that could help us to better understand what happened. We'll get back to you as soon as we can!\n\n\n\n"
+        let txtFileContents = "\(ErrorLogHeaders.error)\(withMessage)\(ErrorLogHeaders.ffprobe)\(withFfprobeOutput)"
+        
+        let txtFile = self.writeTempTxtFile(txtFileContents)
+        
         let service = NSSharingService(named: NSSharingService.Name.composeEmail)
         service?.recipients = ["hello@airtv.io"]
         service?.subject = "Help: Video Converter Error"
-        service?.perform(withItems: ["[Enter your message here]\n\n", "----------------------------------------------------\nDO NOT EDIT THE SECTION BELOW\n----------------------------------------------------\n\n", "Error message:\n\n\(withMessage)\n\n",
-                                     "FFProbe output:\n\n\(withFfprobeOutput)"])
+        service?.perform(withItems: [messageContents, txtFile])
       }
       if modalResponse == NSApplication.ModalResponse.alertSecondButtonReturn {
         print("User did dismiss error message")
@@ -34,4 +38,32 @@ extension ViewController {
       }
     })
   }
+  
+  func writeTempTxtFile(_ contents: String) -> URL {
+    let url = FileManager.default.temporaryDirectory
+      .appendingPathComponent("Error-log-\(UUID().uuidString)")
+      .appendingPathExtension("txt")
+    let string = contents
+    try? string.write(to: url, atomically: true, encoding: .utf8)
+    return url
+  }
+}
+
+struct ErrorLogHeaders {
+  
+  static let error = """
+######################
+### ERROR CONTENTS ###
+######################
+\n\n
+"""
+  
+  static let ffprobe = """
+\n\n\n\n
+######################
+### FFPROBE OUTPUT ###
+######################
+\n\n
+"""
+  
 }
