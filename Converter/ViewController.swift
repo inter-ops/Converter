4c71d93b52349c5f7c5870cc8ce04c51b7b52e94
Justin Bush <revblaze@users.noreply.github.com>
2022-09-10 21:33:26 -0600

    feat: open file from ViewController for menu control

--- Converter/ViewController.swift
+++ Converter/ViewController.swift
@@ -39,6 +39,23 @@ class ViewController: NSViewController, NSPopoverDelegate, DragDropViewDelegate 
     displayClearButton(.hide)
   }
   
+  func openFileMenuItem() {
+    let openPanel = NSOpenPanel()
+    
+    openPanel.allowsMultipleSelection = false
+    openPanel.canChooseDirectories = true
+    openPanel.canCreateDirectories = true
+    openPanel.canChooseFiles = true
+    openPanel.allowedFileTypes = supportedFormats
+    
+    let response = openPanel.runModal()
+    if response == .OK {
+      let path = openPanel.url!.absoluteString
+      print("path: \(path)")
+      dragDropViewDidReceive(fileUrl: path)
+    }
+  }
+  
   override func viewDidAppear() {
     // Handles opening of file in application on launch after initial load
     DispatchQueue.main.async {
