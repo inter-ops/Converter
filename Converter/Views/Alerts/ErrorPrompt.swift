19a9b7f1e7b9c561763fa1274db14a59415a8dd4
Francesco Virga <francescod.virga@gmail.com>
2022-09-12 21:25:01 +0200

    fix: show error message for same file name error

--- Converter/Views/Alerts/ErrorPrompt.swift
+++ Converter/Views/Alerts/ErrorPrompt.swift
@@ -10,7 +10,7 @@ import Cocoa
 extension ViewController {
   
   /// Alert user of an error that occured, with the option of forwarding to devs
-  func alertErrorPrompt(withMessage: String, withFfprobeOutput: String, withFfmpegCommand: String) {
+  func unexpectedErrorAlert(withErrorMessage: String, withFfprobeOutput: String, withFfmpegCommand: String) {
     let a = NSAlert()
     a.messageText = "An error occured"
     a.informativeText = "There was a problem converting your file. Would you like to send this error to the dev team?"
@@ -23,10 +23,10 @@ extension ViewController {
         print("User did choose to send error message")
         // Format message body and log contents to be used in email
         let messageContents = "[Enter any additional details here]\n\n\n\n"
-        let txtFileContents = "Command: \(withFfmpegCommand)\(ErrorLogHeaders.error)\(withMessage)\(ErrorLogHeaders.ffprobe)\(withFfprobeOutput)"
+        let txtFileContents = "Command: \(withFfmpegCommand)\(ErrorLogHeaders.error)\(withErrorMessage)\(ErrorLogHeaders.ffprobe)\(withFfprobeOutput)"
         print("TEXT: \(txtFileContents)")
         // Create an error log txt file to use as attachment
-        let txtFile = self.writeTempTxtFile(txtFileContents)
+        let txtFile = writeTempTxtFile(txtFileContents)
         // Compose mail client request with message and log contents
         let service = NSSharingService(named: NSSharingService.Name.composeEmail)
         service?.recipients = ["hello@airtv.io"]
@@ -39,21 +39,30 @@ extension ViewController {
       }
     })
   }
-  /// Input console log String; return `.txt` file with temporary address to be used as an attachment
-  /// - Parameters:
-  ///   - contents: Console log contents as a String
-  /// - Returns: URL reference of temporary `.txt` file
-  func writeTempTxtFile(_ contents: String) -> URL {
-    let url = FileManager.default.temporaryDirectory
-      .appendingPathComponent("error-log-\(UUID().uuidString)")
-      .appendingPathExtension("txt")
-    let string = contents
-    try? string.write(to: url, atomically: true, encoding: .utf8)
-    return url
+  
+  func errorAlert(withMessage: String) {
+    let a = NSAlert()
+    a.messageText = "An error occured"
+    a.informativeText = withMessage
+    a.addButton(withTitle: "Ok")
+    a.alertStyle = NSAlert.Style.critical
+    
+    a.beginSheetModal(for: self.view.window!)
   }
 }
 
-
+/// Input console log String; return `.txt` file with temporary address to be used as an attachment
+/// - Parameters:
+///   - contents: Console log contents as a String
+/// - Returns: URL reference of temporary `.txt` file
+func writeTempTxtFile(_ contents: String) -> URL {
+  let url = FileManager.default.temporaryDirectory
+    .appendingPathComponent("error-log-\(UUID().uuidString)")
+    .appendingPathExtension("txt")
+  let string = contents
+  try? string.write(to: url, atomically: true, encoding: .utf8)
+  return url
+}
 
 // MARK: Error Log Headers
 struct ErrorLogHeaders {
@@ -72,8 +81,6 @@ struct ErrorLogHeaders {
 \n\n
 """
 }
-
-
 
 // MARK: Error Test Data
 // Sample Error for testing prompt reporting
@@ -96,7 +103,7 @@ extension ViewController {
         let messageContents = "[Enter any additional details here]\n\n\n\n"
         let txtFileContents = AlertErrorTest.sampleOutput
         // Create an error log txt file to use as attachment
-        let txtFile = self.writeTempTxtFile(txtFileContents)
+        let txtFile = writeTempTxtFile(txtFileContents)
         // Compose mail client request with message and log contents
         let service = NSSharingService(named: NSSharingService.Name.composeEmail)
         service?.recipients = ["hello@airtv.io"]
