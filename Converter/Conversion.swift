45f7d64c84595a1858e4932fe69549743f4e38ce
Francesco Virga <francescod.virga@gmail.com>
2022-09-16 15:37:24 +0200

    refactor: change crf to 26

--- Converter/Conversion.swift
+++ Converter/Conversion.swift
@@ -51,7 +51,7 @@ func getVideoConversionCommand(inputFilePath: String, outputFilePath: String) ->
     // - https://superuser.com/questions/1586934/vp9-encoding-with-ffmpeg-relation-between-speed-and-deadline-options
     // - https://superuser.com/questions/556463/converting-video-to-webm-with-ffmpeg-avconv
     // - https://superuser.com/a/1280369
-    return "-c:v libvpx -b:v 5M -deadline good -cpu-used 2 -crf 10"
+    return "-c:v libvpx -b:v 5M -deadline good -cpu-used 2 -crf 26"
     
   case VideoFormat.mp4.rawValue, VideoFormat.mov.rawValue, VideoFormat.m4v.rawValue, VideoFormat.mkv.rawValue:
     // If input file is WEBM, we re-encode to H264
