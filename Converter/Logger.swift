54516ea947520c3ab5beb7ebb306f96d9666dc32
Francesco Virga <francescod.virga@gmail.com>
2022-10-04 19:04:23 -0400

    feat: start setting up levels

--- Converter/Logger.swift
+++ Converter/Logger.swift
@@ -16,36 +16,37 @@ import OSLog
 // - Overview of logging options: https://stackoverflow.com/questions/25951195/swift-print-vs-println-vs-nslog
 // FFMPEG kit overview (discusses logging): https://tanersener.medium.com/introduction-to-ffmpeg-kit-api-59623911fbfc
 
-// Next up: start testing out the logger to weed out implementation issues
-
 let subsystem = Bundle.main.bundleIdentifier!
 
 // TODO: Make available for older platforms
+
+// TODO: Create wrapper func for logging so that we can pass generic strings
+
 @available(macOS 11.0, *)
-func ini() {
+struct ApplicationLogger {
+  static let applicationLogger = Logger(subsystem: subsystem, category: "application")
   
-  let ffmpepLogger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "ffmpeg-kit")
+  static let ffmpepLogger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "ffmpeg-kit")
   
-  // TODO: If we want to simply take all print statemnts and send to a file, we should set alwaysPrintLogs and remove the enableLogcallback all together. Otherwise, we can capture all ffmpeg logs here, but silence them from being printed to main console and capture those separately.
-  // See here for more info https://tanersener.medium.com/introduction-to-ffmpeg-kit-api-59623911fbfc
-  //    FFmpegKitConfig.setLogRedirectionStrategy(.alwaysPrintLogs)
-  FFmpegKitConfig.enableLogCallback { log in
-    switch log!.getLevel() {
-      case Level.avLogWarning:
-        ffmpepLogger.warning("\(log!.getSessionId()): \(log!.getMessage()!)")
-      case Level.avLogInfo:
-        ffmpepLogger.info("\(log!.getSessionId()): \(log!.getMessage()!)")
-      case Level.avLogError:
-        ffmpepLogger.error("\(log!.getSessionId()): \(log!.getMessage()!)")
-      case Level.avLogFatal, Level.avLogPanic:
-        ffmpepLogger.critical("\(log!.getSessionId()): \(log!.getMessage()!)")
+  static func initFfmpegLogs() {
+    
+    FFmpegKitConfig.enableLogCallback { log in
+      // Note: if we find logs from various ffmpeg processes confusing, we can also log session ID with log!.getSessionId()
+      
+      switch log!.getLevel() {
+      case 24: // LevelAVLogWarning
+        ffmpepLogger.warning("\(log!.getMessage()!)")
+      case 32: // LevelAVLogInfo
+        ffmpepLogger.info("\(log!.getMessage()!)")
+      case 16: // LevelAVLogError
+        ffmpepLogger.error("\(log!.getMessage()!)")
+      case 8, 0: // LevelAVLogFatal, LevelAVLogPanic
+        ffmpepLogger.critical("\(log!.getMessage()!)")
       default:
-        ffmpepLogger.log("\(log!.getSessionId()): \(log!.getMessage()!)")
+        ffmpepLogger.log("\(log!.getMessage()!)")
+      }
     }
   }
-  
-  // TODO: Expose this and replace all print statements with it.
-  let applicationLogger = Logger(subsystem: subsystem, category: "application")
 }
 
 @available(macOS 12.0, *)
