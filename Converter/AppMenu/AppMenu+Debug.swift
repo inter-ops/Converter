b97899a1416f7982aedb9cc3b86f9657901f9c88
Justin Bush <revblaze@users.noreply.github.com>
2022-11-04 10:13:39 -0600

    feat: PurchasePremium sheet segue, Debug menu item

--- Converter/AppMenu/AppMenu+Debug.swift
+++ Converter/AppMenu/AppMenu+Debug.swift
@@ -23,4 +23,9 @@ extension AppDelegate {
     viewController?.toggleExpandCollapsePremiumView(self)
   }
   
+  @IBAction func togglePurchasePremiumView(_ sender: NSMenuItem) {
+    let viewController = mainWindow.contentViewController as? ViewController
+    viewController?.segue(.sheetPurchasePremium)
+  }
+  
 }
---
+++ Converter/Views/Premium/PurchasePremium/ViewController+Purchase.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController+Purchase.swift
+//  Converter
+//
+//  Created by Justin Bush on 11/4/22.
+//
+
+import Cocoa
+
+extension ViewController {
+  
+  func openPurchasePremiumView() {
+    segue(.sheetPurchasePremium)
+  }
+  
+}
