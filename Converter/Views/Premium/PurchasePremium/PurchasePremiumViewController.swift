845317c94d9f69ee5b5759cd2f242a3f168c8c59
Justin Bush <revblaze@users.noreply.github.com>
2022-11-04 10:51:43 -0600

    feat: hook IB outlets

--- Converter/Views/Premium/PurchasePremium/PurchasePremiumViewController.swift
+++ Converter/Views/Premium/PurchasePremium/PurchasePremiumViewController.swift
@@ -10,12 +10,24 @@ import WebKit
 
 class PurchasePremiumViewController: NSViewController {
   
+  @IBOutlet weak var mainView: NSView!
+  // 1. Top View: Logo and Dismiss Button
+  @IBOutlet weak var premiumLogoView: NSView!
+  @IBOutlet weak var dismissViewButton: NSButton!
+  // 2. Top-Middle View: Checkmark Details
+  @IBOutlet weak var premiumDetailsView: NSView!
   @IBOutlet weak var premiumDetailsImageView: NSImageView!
-  
+  // 3. Bottom-Middle View: Video Preview
+  @IBOutlet weak var videoPreviewView: NSView!
+  @IBOutlet weak var presentVideoPreviewButton: NSButton!
+  @IBOutlet weak var dismissVideoPreviewButton: NSButton!
+  @IBOutlet weak var videoPreviewWebView: WKWebView!
+  @IBOutlet weak var videoPreviewHeightConstraint: NSLayoutConstraint!
+  // 4. Bottom: Action Buttons
+  @IBOutlet weak var actionRowView: NSView!
   @IBOutlet weak var purchasePremiumButton: NSButton!
   @IBOutlet weak var restorePurchaseButton: NSButton!
-  
-  @IBOutlet weak var previewPremiumVideoWebView: WKWebView!
+
   
   override func viewDidLoad() {
     super.viewDidLoad()
