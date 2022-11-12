5496c65cf2741e4579cb45465d4e327efb572ed7
Justin Bush <revblaze@users.noreply.github.com>
2022-11-12 11:33:49 -0700

    refactor: ProRes VideoQuality cases

--- Converter/VideoFormat/VideoQuality.swift
+++ Converter/VideoFormat/VideoQuality.swift
@@ -8,29 +8,45 @@
 enum VideoQuality: String, CaseIterable {
   case betterQuality, balanced, smallerSize
   
+  case prAuto, prProxy, prLt, prStandard, prHq, pr4444, prXq
+  
   var dropdownTitle: String {
     switch self {
+    // Generic
     case .betterQuality: return "Better Quality"
     case .balanced: return "Balanced"
     case .smallerSize: return "Smaller Size"
+    // ProRes
+    case .prAuto: return "Auto"
+    case .prProxy: return "ProRes 422 Proxy"
+    case .prLt: return "ProRes 422 LT"
+    case .prStandard: return "ProRes 422"
+    case .prHq: return "ProRes 422 HQ"
+    case .pr4444: return "ProRes 4444"
+    case .prXq: return "ProRes 4444 XQ"
+    }
+  }
+}
+
+extension VideoCodec {
+  
+  var qualityTypes: [VideoQuality] {
+    switch self {
+    case .h264: return [.betterQuality, .balanced, .smallerSize]
+    case .hevc: return [.betterQuality, .balanced, .smallerSize]
+    case .vp8: return [.betterQuality, .balanced, .smallerSize]
+    case .vp9: return [.betterQuality, .balanced, .smallerSize]
+    case .mpeg4: return [.betterQuality, .balanced, .smallerSize]
+    case .gif: return [.betterQuality, .balanced, .smallerSize]
+    case .prores: return [.prAuto, .prProxy, .prLt, .prStandard, .prHq, .pr4444, .prXq]
+    // Will not appear in codec dropdown
+    case .mpeg1video: return [.betterQuality, .balanced, .smallerSize]
+    case .mpeg2video: return [.betterQuality, .balanced, .smallerSize]
+    case .unknown: return [.betterQuality, .balanced, .smallerSize]
     }
   }
   
-  enum ProRes: String, CaseIterable {
-    case auto, proxy, lt, standard, hq, res4444, xq
-    
-    var dropdownTitle: String {
-      switch self {
-      case .auto: return "Auto"
-      case .proxy: return "ProRes 422 Proxy"
-      case .lt: return "ProRes 422 LT"
-      case .standard: return "ProRes 422"
-      case .hq: return "ProRes 422 HQ"
-      case .res4444: return "ProRes 4444"
-      case .xq: return "ProRes 4444 XQ"
-      }
     }
-    
   }
   
 }
