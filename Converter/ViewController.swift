79150e6daa66b4a0e36d3c9c80a94ea92de0c703
Francesco Virga <francescod.virga@gmail.com>
2022-11-21 23:10:11 -0500

    feat: dont promt error when cancelling file selection

--- Converter/ViewController.swift
+++ Converter/ViewController.swift
@@ -445,7 +445,6 @@ class ViewController: NSViewController, NSPopoverDelegate, DragDropViewDelegate 
       if inputVideos.count > 1 {
         let userSelectedOutputDirectory = selectOutputDirectory()
         if userSelectedOutputDirectory == nil {
-          self.errorAlert(withMessage: "You must select an output folder.")
           return
         }
         
@@ -522,7 +521,6 @@ class ViewController: NSViewController, NSPopoverDelegate, DragDropViewDelegate 
       else {
         let userSelectedOutputFileUrl = selectOutputFileUrl(format: outputFormat, inputFileUrl: inputVideos[0].fileUrl)
         if userSelectedOutputFileUrl == nil {
-          self.errorAlert(withMessage: "You must select an output folder.")
           return
         }
         inputVideos[0].outputFileUrl = userSelectedOutputFileUrl
@@ -575,6 +573,7 @@ class ViewController: NSViewController, NSPopoverDelegate, DragDropViewDelegate 
       DispatchQueue.main.async {
         // Reference: https://github.com/tanersener/ffmpeg-kit-test/blob/main/macos/test-app-cocoapods/FFmpegKitMACOS/CommandViewController.m
         
+        // TODO: After a user cancels, we should be able to pick up the conversion from where it ended. Could do this pretty easily by leaving activeVideoIndex set if user cancels conversion.
         if returnCode!.isValueCancel() {
           self.updateProgressBar(value: 0)
           self.estimatedTimeText.stringValue = "Canceled ⚠️"
